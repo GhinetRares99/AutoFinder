2c2b768b2bdb1a0a036932f0a872e223cc27ad10
Ghinet Rares <rares.ghinet@yahoo.com>
2022-06-09 23:43:22 +0300

    Final commit

--- AutoFinder/src/app/Components/account/account.component.ts
+++ AutoFinder/src/app/Components/account/account.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { DeleteAccountService } from 'src/app/Services/delete-account.service';
-import { EditAccountService } from 'src/app/Services/edit-account.service';
 
 @Component({
   selector: 'app-account',
@@ -16,7 +15,6 @@ export class AccountComponent implements OnInit {
 
   ngOnInit(): void {
     this.session_user = JSON.parse(sessionStorage.getItem('c_user') as string)
-    //console.log(JSON.parse(sessionStorage.getItem('c_user') as string))
   }
 
   DeleteUser(){
